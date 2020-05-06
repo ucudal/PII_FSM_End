14284b6c760634e22910ab869eaca9d07c3caac0
Nicolás Alliaume <nicolasalliaume@gmail.com>
2020-05-05 21:38:26 -0300

    updated netcoreapp and tokens

--- src/Library/Singleton/CognitiveApiSingleton.cs
+++ src/Library/Singleton/CognitiveApiSingleton.cs
@@ -4,7 +4,7 @@ namespace RideShareUCU
 {
     public static class CognitiveApiSingleton
     {
-        private const string apiKey = "a36648d3c5134ab692acd35605d491f7";
+        private const string apiKey = "620e818a46524ceb92628cde08068242";
         private static CognitiveFace singleton = null;
         public static CognitiveFace Singleton
         {
--- src/Library/Singleton/TwitterApiSingleton.cs
+++ src/Library/Singleton/TwitterApiSingleton.cs
@@ -3,12 +3,11 @@ using TwitterUCU;
 namespace RideShareUCU
 {
     public static class TwitterApiSingleton
-    {
-        
-        private const string consumerKey = "dtOgpyjBBXglAzMEjMMZtFf73";
-        private const string consumerKeySecret = "Qzm0FxotJ9YyoXiGLJ4JI9IZFWmYvB4LWpteWPGVYofxSG4FnN";
-        private const string accessTokenSecret = "HXtlP1SRnJCL5a37R98hFrIRlEIouZX3Ra4s6JuFOpXZF";
-        private const string accessToken = "1396065818-13uONd7FgFVXhW1xhUCQshKgGv4UOnKeDipg4cz";
+    {       
+        private const string consumerKey = "g7rkPB5uI2xOqELAhlNrorSU4";
+        private const string consumerKeySecret = "8hOTyS71GrTH9Ool3rXykAJRY5AmgSPiy78b1wYUPcvfIzXeEc";
+        private const string accessTokenSecret = "675fHmUzeaPajtj3pO64w5xd3p9YI3kco7kSvKhzeEvYe";
+        private const string accessToken = "1396065818-8vnV9HJFW5ArcfFg2zE9hLA68CZYFXO8Cjv6o2E";
         private static TwitterImage singleton = null;
         public static TwitterImage Singleton
         {
